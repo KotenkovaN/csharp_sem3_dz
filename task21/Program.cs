958e550c1531988a226ae8794b5531eaecc3b214
Nataly <natalya-kotenkova@yandex.ru>
2022-12-01 17:05:48 +0100

    added task21

---
+++ task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear(); 
+Console.Write("Введите координату X1: "); 
+double X1 = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Введите координату Y1: "); 
+double Y1 = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Введите координату Z1: "); 
+double Z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X2: "); 
+double X2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y2: "); 
+double Y2 = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Введите координату Z2: "); 
+double Z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(X1-X2,2) +  Math.Pow(Y1-Y2,2) + Math.Pow(Z1-Z2,2))); 
